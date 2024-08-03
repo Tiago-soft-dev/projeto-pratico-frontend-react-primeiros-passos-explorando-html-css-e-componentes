235eb4ae531466f7e330df01168a95a59fcec1f4
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-02 21:30:55 -0300

    Criação e estilização do componente `Card`.

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,12 @@
+import './Card.css' 
+
+export default function Card(props){
+    return (
+        <>
+            <div className="card">
+                <h2>Girassol</h2>
+                <img src="https://cdn.pixabay.com/photo/2023/10/30/05/19/sunflowers-8351807_640.jpg" alt='girassol' />
+            </div>
+        </>
+    )
+}
