da5cae99840c3fabff9fe5f9868f1c3c31ac6ef3
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-08-02 21:37:32 -0300

    Configuração e estilização do componente `<Header />`.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Card from './Components/Card/Card'
+import Header from './Components/Header/Header'
 
 function App() {
   
 
   return (
     <>
+    <Header />
      <Card  />
      
     </>
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+
+export default function Header(){
+    return (
+        <div className="header">
+            <h1>Bem vindo ao Flor-Na-Net Brasil</h1>
+            <p>Aqui você encontra imagens da flora nacional</p>
+        </div>
+    )
+}
